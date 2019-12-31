7ab3ad0ae3324d77080d41a017a03a5454b4de46
François Roussel <franco.roussel@gmail.com>
2019-12-30 20:50:59 -0500

    Ajout de la configuration datasource aux propriétés de l'application.@

---
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -0,0 +1,38 @@
+package guru.springframework.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import guru.springframework.examplebeans.FakeDataSource;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+	@Value("${guru.username}")
+	String username;
+	
+	@Value("${guru.password}")
+	String password;
+	
+	@Value("${guru.dburl}")
+	String url;
+	
+	@Bean
+	public FakeDataSource fakeDataSource() {
+		FakeDataSource fakeDataSource = new FakeDataSource();
+		fakeDataSource.setPassword(password);
+		fakeDataSource.setUsername(username);
+		fakeDataSource.setUrl(url);
+		return fakeDataSource;
+	}
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer properties() {
+		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+		return propertySourcesPlaceholderConfigurer;
+	}
+}
